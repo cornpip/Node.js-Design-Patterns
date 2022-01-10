57d14a2a1b806ed02c0157b1f0fb4f38be22d86f
cornpip <chdnjf1375@naver.com>
2022-01-10 23:22:53 +0900

    03-04 예제

--- 03-callbacks-and-events/04-an-unpredictable-function/README.md
+++ 03-callbacks-and-events/04-an-unpredictable-function/README.md
@@ -9,3 +9,32 @@ To run the example launch:
 ```bash
 node index.js
 ```
+# function flow
+`createFileReader(data.txt)` 실행,  
+cb함수의 인자로 data가 들어가며 실행  cache에 없으므로 readFile로 실행(비동기)  
+여기서 비동기 함수라 제어권 넘겨주고 `onDataReady` 함수부터 실행  
+`onDataReady` 함수에 인자 함수를 전달해 listeners Array에 함수 추가  
+이제 `onDataReady` 함수 끝났고 비동기 작업(readFile) 반환 후 실행  
+cb함수 실행
+```bash
+top -->
+[ [Function (anonymous)] ]
+```
+console 2개 진행되고 listeners에 들어간 함수에 data 인자로 실행됨
+```bash
+First call data: some data yap~
+```
+콘솔 찍히고 `createFileReader(data.txt)` reader2 실행됨, 이번에는 cache에 값이 있으므로 readFile __(비동기)__ 가 아닌 바로 cb함수 __(동기)__ 실행 cb함수가 실행되고 동기이므로 제어권 그대로 진행
+```bash
+top -->
+[]
+bottom -->
+sync
+```
+남은 2개 console 마저 출력 후 readFile함수 마침
+```bash
+bottom -->
+[ [Function (anonymous)] ]
+async
+```
+비동기 작동의 이해보다도 함수형 프로그래밍스러운 코드 흐름이 더 어려웠다.
--- 03-callbacks-and-events/04-an-unpredictable-function/index.js
+++ 03-callbacks-and-events/04-an-unpredictable-function/index.js
@@ -1,4 +1,5 @@
 /* eslint handle-callback-err: 0 */
+
 import { readFile } from 'fs'
 
 const cache = new Map()
@@ -7,11 +8,13 @@ function inconsistentRead (filename, cb) {
   if (cache.has(filename)) {
     // invoked synchronously
     cb(cache.get(filename))
+    console.log('sync');
   } else {
     // asynchronous function
     readFile(filename, 'utf8', (err, data) => {
       cache.set(filename, data)
       cb(data)
+      console.log('async')
     })
   }
 }
@@ -19,7 +22,11 @@ function inconsistentRead (filename, cb) {
 function createFileReader (filename) {
   const listeners = []
   inconsistentRead(filename, value => {
+    console.log(`top -->`);
+    console.log(listeners);
     listeners.forEach(listener => listener(value))
+    console.log(`bottom -->`);
+    console.log(listeners);
   })
 
   return {
@@ -30,11 +37,11 @@ function createFileReader (filename) {
 const reader1 = createFileReader('data.txt')
 reader1.onDataReady(data => {
   console.log(`First call data: ${data}`)
-
   // ...sometime later we try to read again from
   // the same file
   const reader2 = createFileReader('data.txt')
   reader2.onDataReady(data => {
     console.log(`Second call data: ${data}`)
+    console.log(`--------------`);
   })
 })
