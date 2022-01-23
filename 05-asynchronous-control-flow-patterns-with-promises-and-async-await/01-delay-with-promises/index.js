baec06a9e2fb88fc611185daf2413266647dd625
cornpip <chdnjf1375@naver.com>
2022-01-24 01:08:45 +0900

    프라미스 실행시점, await실행시점

--- 05-asynchronous-control-flow-patterns-with-promises-and-async-await/01-delay-with-promises/index.js
+++ 05-asynchronous-control-flow-patterns-with-promises-and-async-await/01-delay-with-promises/index.js
@@ -20,36 +20,43 @@ function delay (milliseconds) {
 //     setTimeout(()=> console.log('aaaaaaa'), 1000)
 //   })
 
+// for (i=0; i<3; i++){
+//   delay();
+// };
+
+// const a = console.log('aaa예요');
+
   //이렇게 then에 process.nexttick있어도 뒤에 then이 더 먼저다
 
 // process.nextTick(()=> console.log('11223333'));
 
 const a = new Promise((resolve, reject) => {
+  console.log('111');
   setTimeout(() => {
     // resolve(new Date())
     resolve('전달값이여여ㅕㅕ여여');
-    console.log('hi')
-  }, 1000)
+    console.log('hello')
+  }, 1)
 });
 
-const b = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve(new Date())
-    resolve('전달값bbbbbbbbbb');
-    console.log('hibbbbbbb')
-  }, 1000)
-});
-
+// const b = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve(new Date())
+//     resolve('전달값bbbbbbbbbb');
+//     console.log('hibbbbbbb')
+//   }, 1000)
+// });
 
 async function test(){
-  console.log('first');
   for (i=0; i<3; i++){
-    console.log('rr');
     await a; //프라미스의 실행? 은 한번인듯 resolve는 3번도는데 console.log('hi')는 한번돔
-    await b;
-    console.log('bb');
+    // await a;
+    console.log('1111111111111111');
   }
 }
 
+// a;
+// a;
+
 test();
-console.log('과연');
+// console.log('과연');
